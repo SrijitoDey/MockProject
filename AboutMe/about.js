0e09f154d01ec7a1f843bac728cf5899882d62bf
SrijitoDey <123385018+SrijitoDey@users.noreply.github.com>
2023-03-23 15:17:06 +0530

    Add files via upload

---
+++ AboutMe/about.js
@@ -0,0 +1,34 @@
+import './about.css'
+
+function AboutMe() {
+
+
+    return (
+        <div id='main-container'>
+
+            <div id='div-o'>
+
+                <div id='image-box'>
+                    <img id='photo' src="https://th.bing.com/th/id/OIP.OOh0PWJjoab-C0l3WQs4ugHaLL?pid=ImgDet&rs=1" />
+                </div>
+
+                <div id='div-i'>
+
+                    <div id='heading'>
+                        <h3 id='head-text'>
+                            About Me.
+                        </h3>
+                    </div>
+
+                    <div id='about-section'>
+                        <p id='text-about'>This is for summary</p>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    );
+}
+
+export default AboutMe;
---
+++ Home/home.js
@@ -0,0 +1,27 @@
+import './home.css'
+
+function Home() {
+    return (
+        <div id='main-cont'>
+
+            <div id='res-back'>
+                <div id='cover'>
+                </div>
+            </div>
+
+            <div id='text-cont'>
+                <div id='info-box'>
+                <h1 id='author'>Test Name</h1>
+                <h4 id='res-text'>.. MY INTERACTIVE RESUME</h4>
+                
+                <a id='prof-sel' href='#'>(Choose A Different Profile)</a>    
+                </div>
+            </div>
+
+
+        </div>
+    );
+
+}
+
+export default Home; 
