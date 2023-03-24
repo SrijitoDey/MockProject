26754584bb5b9b3a8910c0647dc6f5e6b277e5c2
rrn04 <63703526+rrn04@users.noreply.github.com>
2023-03-24 10:33:00 +0530

    Update contact.jsx

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -7,85 +7,88 @@ import { TelephoneFill } from "react-bootstrap-icons";
 import { Github } from "react-bootstrap-icons";
 import { Linkedin } from "react-bootstrap-icons";
 
-const Contact = () => {
-  const form = useRef();
+import "./contact.css"
 
-  const sendEmail = (e) => {
-    e.preventDefault();
+function Contact({selectedProfile}){
+    const form = useRef();
 
-    emailjs
-      .sendForm(
-        "service_913brwe",
-        "template_t8ll2oh",
-        form.current,
-        "jbPaCUmqXSdu34eYX"
-      )
-      .then(
-        (result) => {
-          console.log(result.text);
-          console.log("message sent");
-        },
-        (error) => {
-          alert(error.text);
-        }
-      );
-  };
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs
+            .sendForm(
+                "service_913brwe",
+                "template_t8ll2oh",
+                form.current,
+                "jbPaCUmqXSdu34eYX"
+            )
+            .then(
+                (result) => {
+                    console.log(result.text);
+                    console.log("message sent");
+                },
+                (error) => {
+                    alert(error.text);
+                }
+            );
+    };
 
 
-  return (  
-    <div className="container">
+    return (
+        <div className="container contact" id="contact">
 
-    <div className="div_contact_me">
-      <h3 style={{"color":"white"}}>Contact me</h3><br />
+            <div className="div_contact_me">
+                <h3 style={{ "color": "white" }}>Contact me</h3><br />
 
-      <div>      
-      <a className="link_of__icon_and_lable" href="mailto:rranjithnair04@gmail.com"><Envelope size={20} /> &nbsp; Email Address</a><br />
-      &nbsp; &nbsp; &nbsp; &nbsp; <span className="span_contact_me">rranjithnair04@gmail.com</span>
-      </div> <br />
+                <div>
+                    {console.log(selectedProfile)}
+                    <a className="link_of__icon_and_lable" href={"mailto:" + selectedProfile.contact.email}><Envelope size={20} /> &nbsp; Email Address</a><br />
+                    &nbsp; &nbsp; &nbsp; &nbsp; <span className="span_contact_me">{selectedProfile.contact.email}</span>
+                </div> <br />
 
-      <div>      
-      <a className="link_of__icon_and_lable" href="#"><Telegram color="white" size={20} /> &nbsp; Telegram</a><br />
-      &nbsp; &nbsp; &nbsp; &nbsp; <span className="span_contact_me">rrn_rahul</span>
-      </div><br />
+                <div>
+                    <a className="link_of__icon_and_lable" href={selectedProfile.contact.telegram}><Telegram color="white" size={20} /> &nbsp; Telegram</a><br />
+                    &nbsp; &nbsp; &nbsp; &nbsp; <span className="span_contact_me">{selectedProfile.contact.telegram}</span>
+                </div><br />
 
-      <div>      
-      <a className="link_of__icon_and_lable" href="#"><TelephoneFill color="white" size={20} /> &nbsp; Phone</a><br />
-      &nbsp; &nbsp; &nbsp; &nbsp; <span className="span_contact_me">+91 8104651040</span>
-      </div><br />
+                <div>
+                    <a className="link_of__icon_and_lable" href="#"><TelephoneFill color="white" size={20} /> &nbsp; Phone</a><br />
+                    &nbsp; &nbsp; &nbsp; &nbsp; <span className="span_contact_me">+91 {selectedProfile.contact.phone}</span>
+                </div><br />
 
-      <div>      
-      <a className="link_of__icon_and_lable" href="https://github.com/rrn04"><Github color="white" size={20} /> &nbsp; Github</a><br />
-      &nbsp; &nbsp; &nbsp; &nbsp; <span className="span_contact_me">rrn04</span>
-      </div><br />
+                <div>
+                    <a className="link_of__icon_and_lable" href={selectedProfile.social.github}><Github color="white" size={20} /> &nbsp; Github</a><br />
+                    &nbsp; &nbsp; &nbsp; &nbsp; <span className="span_contact_me">{selectedProfile.social.github}</span>
+                </div><br />
 
-      <div>      
-      <a className="link_of__icon_and_lable" href="https://www.linkedin.com/in/rahul-nair-7202211aa/"><Linkedin color="white" size={20} /> &nbsp; LinkedIn</a><br />
-      &nbsp; &nbsp; &nbsp; &nbsp; <span className="span_contact_me">rahul-nair-7202211aa/</span>
-      </div>
+                <div>
+                    <a className="link_of__icon_and_lable" href={selectedProfile.social.linkedin}><Linkedin color="white" size={20} /> &nbsp; LinkedIn</a><br />
+                    &nbsp; &nbsp; &nbsp; &nbsp; <span className="span_contact_me">{selectedProfile.social.linkedin.substring(28,30)}</span>
+                </div>
 
-    </div>  
+            </div>
 
-    <div className="outer_get_in_touch">
-      <div className="inner_get_in_touch"><br />
-    <h1 className="hover-underline-animation" style={{"textAlign":"center"}}>
-    Get in touch  
-  </h1>
-      <form ref={form} onSubmit={sendEmail}>
-        <label>Name</label>
-        <input type="text" name="user_name" />
-        <label>Email</label>
-        <input type="email" name="user_email" />
-        <label>Message</label>
-        <textarea name="message" />
-        <div style={{"textAlign":"center"}}>
-        <input type="submit" value="Send" />
-        </div> <br/>
-      </form>
-      </div> 
-    </div>
+            <div className="outer_get_in_touch">
+                <div className="inner_get_in_touch"><br />
+                    <h1 className="hover-underline-animation" style={{ "textAlign": "center" }}>
+                        Get in touch
+                    </h1>
+                    <form ref={form} onSubmit={sendEmail}>
+                        <label>Name</label>
+                        <input type="text" name="user_name" />
+                        <label>Email</label>
+                        <input type="email" name="user_email" />
+                        <label>Message</label>
+                        <textarea name="message" />
+                        <div style={{ "textAlign": "center" }}>
+                            <input type="submit" value="Send" />
+                        </div> <br />
+                    </form>
+                </div>
+            </div>
 
-    </div>
-  );
+        </div>
+    );
 };
 
 export default Contact;
